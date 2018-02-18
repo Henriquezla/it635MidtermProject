88512ab65d4d9f17fa3081244d182c170b386d47
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-17 19:13:32 -0500

    Initial Tables

--- createTables.sql
+++ createTables.sql
@@ -1,6 +1,7 @@
 USE Baseball;
 
-DROP TABLE IF EXISTS pitching,players;
+DROP TABLE IF EXISTS team_roster_per_season,batting,pitching,players,teams;
+
 CREATE TABLE players (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     f_name VARCHAR(255) DEFAULT NULL,
@@ -12,7 +13,7 @@ CREATE TABLE players (
 	PRIMARY KEY(id)
 	
 )Engine=InnoDB;
-ALTER TABLE players AUTO_INCREMENT=1001;
+ALTER TABLE players AUTO_INCREMENT=100000;
 
 CREATE TABLE pitching (
     player_id INT UNSIGNED NOT NULL,
@@ -28,3 +29,87 @@ CREATE TABLE pitching (
        ON UPDATE CASCADE
 )Engine=InnoDB;
 
+CREATE TABLE batting (
+    player_id INT UNSIGNED NOT NULL,
+	games_played SMALLINT UNSIGNED DEFAULT NULL,
+	AB SMALLINT UNSIGNED DEFAULT NULL,
+	H SMALLINT UNSIGNED DEFAULT NULL,
+	bat_AVG FLOAT(3,3) DEFAULT NULL,
+	BB SMALLINT UNSIGNED DEFAULT NULL,
+	K SMALLINT UNSIGNED DEFAULT NULL,
+    PRIMARY KEY (player_id),
+    FOREIGN KEY (player_id) REFERENCES players (id)
+       ON DELETE CASCADE
+       ON UPDATE CASCADE
+)Engine=InnoDB;
+
+CREATE TABLE teams (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) DEFAULT NULL,
+	abbreviation VARCHAR(255) DEFAULT NULL,
+	games_played SMALLINT UNSIGNED DEFAULT NULL,
+	games_won SMALLINT UNSIGNED DEFAULT NULL,
+	games_lost SMALLINT UNSIGNED DEFAULT NULL,
+	PRIMARY KEY(id)
+	
+)Engine=InnoDB;
+ALTER TABLE teams AUTO_INCREMENT=10000;
+
+CREATE TABLE seasons (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    season_year SMALLINT UNSIGNED DEFAULT NULL,
+	PRIMARY KEY(id)
+	
+)Engine=InnoDB;
+ALTER TABLE seasons AUTO_INCREMENT=10;
+
+CREATE TABLE team_roster_per_season (
+    player_id INT UNSIGNED NOT NULL,
+	team_id INT UNSIGNED NOT NULL,
+	season_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (player_id,team_id,season_id),
+    FOREIGN KEY (player_id) REFERENCES players (id)
+       ON DELETE CASCADE
+       ON UPDATE CASCADE,
+	FOREIGN KEY (team_id) REFERENCES teams (id)
+       ON DELETE CASCADE
+       ON UPDATE CASCADE,
+	FOREIGN KEY (season_id) REFERENCES seasons (id)
+       ON DELETE CASCADE
+       ON UPDATE CASCADE
+)Engine=InnoDB;
+
+CREATE TABLE game_schedule (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    team_A_id INT UNSIGNED NOT NULL,
+	team_B_id INT UNSIGNED NOT NULL,
+	schd_date DATE DEFAULT NULL,
+	schd_time TIME DEFAULT NULL,
+	town VARCHAR(255) DEFAULT NULL,
+	PRIMARY KEY(id,team_A_id,team_B_id,schd_date,schd_time),
+	FOREIGN KEY (team_A_id) REFERENCES players (id)
+       ON DELETE CASCADE
+       ON UPDATE CASCADE,
+	FOREIGN KEY (team_B_id) REFERENCES teams (id)
+       ON DELETE CASCADE
+       ON UPDATE CASCADE
+	
+)Engine=InnoDB;
+ALTER TABLE game_schedule AUTO_INCREMENT=100;
+
+CREATE TABLE game_results (
+	schd_id SMALLINT UNSIGNED NOT NULL,
+    team_w INT UNSIGNED NOT NULL,
+	team_w_score TINYINT UNSIGNED NOT NULL,
+	team_l INT UNSIGNED NOT NULL,
+	team_l_score TINYINT UNSIGNED NOT NULL,
+	total_innings TINYINT UNSIGNED NOT NULL,
+	PRIMARY KEY(schd_id,team_w,team_l),
+	FOREIGN KEY (team_w) REFERENCES teams (id)
+       ON DELETE CASCADE
+       ON UPDATE CASCADE,
+	FOREIGN KEY (team_l) REFERENCES teams (id)
+       ON DELETE CASCADE
+       ON UPDATE CASCADE
+	
+)Engine=InnoDB;
