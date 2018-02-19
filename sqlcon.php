adab5ebec0ab3a5335b641eca65f78ae7a3a55b2
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-19 11:52:06 -0500

    Testing SQL query

--- sqlcon.php
+++ sqlcon.php
@@ -1,15 +1,25 @@
 
 <?php
-include("account.php");
-	$dbh = mysqli_connect($hostname, $username, $password, $project);
-		if (!$dbh) {
-		    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-		    echo "Debugging error: " . mysqli_connect_errno() . PHP_EOL;
-		    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-			exit;
-		} else {
-			echo "Connection was successful." . PHP_EOL;
-			
-		}
+	include("account.php");
 
+	$dbh = mysqli_connect($hostname, $username, $password, $project);
+	if (!$dbh) {
+		echo "Error: Unable to connect to MySQL." . PHP_EOL;
+		echo "Debugging error: " . mysqli_connect_errno() . PHP_EOL;
+		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+		exit;
+	} else {
+		echo "Connection was successful." . PHP_EOL;
+	}
+	$dataSet = "";
+	$userQuery = "SELECT * FROM players;";
+	$result = $dbh->query($userQuery);
+	
+	while($row = $result->fetch_array()){
+		$rows[] = $row;
+	}
+	
+	foreach($rows as $row){
+		echo[$row];
+	}
 ?>
