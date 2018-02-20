72a8519e056b96ab2c5430f0e4cbf72d7b1256b6
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-19 22:04:50 -0500

    index.php
    removed unnecessary commented code

--- index.php
+++ index.php
@@ -41,15 +41,6 @@
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
           </form>
     
-    <!--	<form class="form-signin" id="recover" role="form">
-            <h3 class="form-signin-heading">Enter email address</h3>
-            <a href="#" id="flipToLogin" class="flipLink">
-              <div id="triangle-topleft"></div>
-            </a>
-            <input type="email" class="form-control" name="loginEmail" id="loginEmail" placeholder="Email address" required autofocus>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Recover password</button>
-          </form> -->
-
         </div> <!-- /container -->
 	</div>
 </div>
