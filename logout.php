ce8495bb1c86a50f7c995345d77c520f37d468d2
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-25 12:24:12 -0500

    working logout

--- logout.php
+++ logout.php
@@ -1,15 +1,11 @@
 <?php
-	session_start();
-	ini_set("display_errors", true);
-	//if (isset($_POST['logout'])) {
-		unset($_SESSION['user']);
-		unset($_SESSION["state"]);
-		unset($_SESSION['admin_rights']);
-		session_unset();
-		session_destroy();
-		header("Location: index.php");
-		exit;
-	//}
-
+	session_start();	
+	unset($_SESSION['user']);
+	unset($_SESSION["state"]);
+	unset($_SESSION['admin_rights']);
+	session_unset();
+	session_destroy();
+	header("Location: index.php");
+	exit;
 
 ?>
