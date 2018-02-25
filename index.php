de91557a9f9643bdf4c189de2cefc0a583f417ac
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-24 21:57:55 -0500

    index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 			$row = array();
 			if($result){
 				$row = $result->fetch_array(MYSQLI_ASSOC);
-				echo crypt($password,$row['pw_hash']).PHP_EOL;
+				echo crypt($pass,$row['pw_hash']).PHP_EOL;
 				echo $row['pw_hash'].PHP_EOL;
 				if(crypt($password,$row['pw_hash'])==$row['pw_hash']){
 					$_SESSION['user'] = $username;
