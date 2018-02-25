eaf7002e8363fb115751e80108c90cd28ac6f5ad
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-25 11:15:03 -0500

    logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,9 @@
 <?php
 	session_start();
 	if (isset($_GET['logout'])) {
+		unset($_SESSION['user']);
+		unset($_SESSION["state"]);
+		unset($_SESSION['admin_rights']);
 		session_unset();
 		session_destroy();
 		header("Location: index.php");
