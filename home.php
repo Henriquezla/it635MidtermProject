68d223aead45ef3bd12dfee3978fa7d60b0d7032
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-25 11:03:04 -0500

    Visual changes to button spacing

--- home.php
+++ home.php
@@ -25,9 +25,9 @@
             <h3 class="form-signin-heading">Pick your choice</h3><br>
             <a href="#" id="flipToRecover" class="flipLink">
             </a>
-            <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-teams">View Teams</button><br>
-			<button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-schedules">View Schedules</button><br>
-			<button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-scores">View Scores</button><br><br><br>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-teams">View Teams</button>
+			<button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-schedules">View Schedules</button>
+			<button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-scores">View Scores</button><br>
 			<button class="btn btn-lg btn-danger btn-block" type="submit" name="btn-scores">Sign Out</button>
 			</div>
           </form>
