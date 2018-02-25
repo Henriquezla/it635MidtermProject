8e1a43b561a3e4d029565bd03dd9b83b033c27ed
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-25 18:37:17 -0500

    Calling sqlcon.php

--- userteams.php
+++ userteams.php
@@ -2,6 +2,7 @@
 	include("functions.php");
 	session_start();
 	gatekeeper('User');
+	require_once 'sqlcon.php';
 	ini_set("display_errors", true);
 	if(isset($_POST['btn-logout']) ) {
 		header("Location: logout.php");
