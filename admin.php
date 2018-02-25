2ff91841ce84e327de8ebc63a42472601473a148
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-25 13:00:29 -0500

    visual changes

--- admin.php
+++ admin.php
@@ -23,7 +23,7 @@
 
           <form class="form-signin" id="userchoice" role="form" method="post" action="" autocomplete="off">
 		  <div style="margin:auto;width: 50%;padding: 40px;">
-            <h3 class="form-signin-heading">Welcome Back, <?php echo $_SESSION['f_name']; ?></h3><br>
+            <h3 class="form-signin-heading">Welcome Back, <?php echo $_SESSION['f_name']; ?></h3>
             <h4 class="form-signin-heading">Pick your choice</h4><br>
             <a href="#" id="flipToRecover" class="flipLink">
             </a>
