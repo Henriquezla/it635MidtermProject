ba0d2ec0d57157e0c2096fbc8e22e82c6c5a8421
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-24 21:24:10 -0500

    index.php

--- index.php
+++ index.php
@@ -35,6 +35,7 @@
 			echo $username;
 			$result = $dbh->query($query);
 			$row = array();
+			echo $result;
 			if($result){
 				echo "valid email<br>";
 				$row = $result->fetch_array(MYSQLI_ASSOC);
