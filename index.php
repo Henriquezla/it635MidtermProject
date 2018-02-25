db883917fe698636fa365484fe9299a4959acd81
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-24 21:22:59 -0500

    index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 			echo "no error <br>";
 			$username = hash('sha256',$email);
 			//$password = crypt($pass,$salt);
-			$query = "SELECT * FROM users WHERE email='$username'";
+			$query = "SELECT * FROM users WHERE email='".$username."'";
 			echo $username;
 			$result = $dbh->query($query);
 			$row = array();
