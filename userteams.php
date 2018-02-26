03c8796f38fc4ae18ffdf52c78a0da25e64bf36c
Henriquezla <Henriquezla@users.noreply.github.com>
2018-02-25 19:40:56 -0500

    home button

--- userteams.php
+++ userteams.php
@@ -34,7 +34,7 @@
 	} */
 	echo '<div style="margin:auto;width:75%;padding:40px;">';
 	echo '<h4 class="form-signin-heading">Teams</h4><br>';
-	echo '<form><button class="btn btn-lg btn-primary btn-block" type="submit" style="width:20%" name="btn-goback">Home</button><br></form>';
+	echo '<form><button class="btn btn-lg btn-primary btn-block" style="width:20%" type="submit" name="btn-goback">Home</button><br></form>';
 	echo '<table class="table table-striped">';
 	echo '<tr>';
 	echo '<th scope="col">Team ID</th>';
