c0fa3f8472a12a6d99fabc7f549646a936881089
Henriquezla <Henriquezla@users.noreply.github.com>
2018-03-03 16:20:10 -0500

    Removed search box if already searched

--- admin_rmvteam.php
+++ admin_rmvteam.php
@@ -162,22 +162,21 @@
 				}
 			?>
 					
-				<h3 class="form-signin-heading">Enter Team Information</h3><br>
 				<a href="#" id="flipToRecover" class="flipLink">
 				</a>
 				
 				<?php
 				if (isset($searchPerformed) ) {
 					
-					echo '<input type="text" class="form-control" name="searchbox" id="searchbox" placeholder="Search teams by name or press Enter to load all teams..."><br>';
+					echo '<br>';
 					
 				}else{
-					
+					echo '<h3 class="form-signin-heading">Enter Team Information</h3><br>';
 					echo '<input type="text" class="form-control" name="searchbox" id="searchbox" placeholder="Search teams by name or press Enter to load all teams..." autofocus><br>';
-					
+					echo '<button class="btn btn-lg btn-primary btn-block" type="search" name="btn-search">Search</button><br><br>';
 				}
 			?>			
-				<button class="btn btn-lg btn-primary btn-block" type="search" name="btn-search">Search</button><br><br>
+				
 				<button class="btn btn-lg btn-danger btn-block" type="button" onclick="window.location.href = 'admin.php' "; name="btn-cancel">Cancel</button>
 			</div>
           </form>
