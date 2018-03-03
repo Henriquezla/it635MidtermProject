d1c6fc078aa10c11bc5c87c19f38bfa47604c843
Henriquezla <Henriquezla@users.noreply.github.com>
2018-03-02 22:21:27 -0500

    var names..

--- admin_addteam.php
+++ admin_addteam.php
@@ -65,7 +65,7 @@
 		
 		
 		if(!$error) {
-			$query = "INSERT INTO teams(name,abbreviation,games_played,games_won,games_lost) VALUES('$name','$abbrv','$gplayed','$gwon','$glost')";
+			$query = "INSERT INTO teams(name,abbreviation,games_played,games_won,games_lost) VALUES('$tname','$abbrv','$gplayed','$gwon','$glost')";
 			
 			$result = $dbh->query($query);
 			$row = array();
