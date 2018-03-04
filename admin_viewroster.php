6aa5cc3992f646a36bb4893a252efb079da907b5
Henriquezla <Henriquezla@users.noreply.github.com>
2018-03-04 14:57:14 -0500

    variable typo

--- admin_viewroster.php
+++ admin_viewroster.php
@@ -11,7 +11,7 @@
 	
 	if(isset($_POST['btn-delete']) ) {
 		$error = false;
-		$teamID = $_POST['id'][0];
+		$teamID = $_POST['rmvID'][0];
 		$query = "SELECT (SELECT name FROM teams WHERE id='$teamID') AS 'Team Name', p1.f_name AS 'First Name',p1.l_name AS 'Last Name',s1.season_year AS 'Season' FROM team_roster_per_season s1 LEFT JOIN players p1 ON s1.player_id = p1.id;";
 		$result = $dbh->query($query);
 		
