7cda0cbc6de28a20646d4234092e2eb1428980f0
Henriquezla <Henriquezla@users.noreply.github.com>
2018-03-04 12:47:11 -0500

    img size

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 	function gatekeeper($type){
 		if ($_SESSION['user']==""){
 			echo '<h3 class="form-signin-heading">You must login to access this page.<br> Redirecting to Login page...</h3><br>';
-			echo '<img src="403.jpg" alt="Cant touch this" style="width:640px;height:480px;border:12;">';
+			echo '<img src="403.jpg" alt="Cant touch this" style="width:132px;height:80px;border:12;">';
 			header("refresh:3; url=index.php");
 			exit();
 			
@@ -11,7 +11,7 @@
 		
 		if ($_SESSION["state"] != $type) {
 			echo "<h3 class='form-signin-heading'>Login as '$type'.<br> Redirecting to Homepage...</h3><br>";
-			echo '<img src="403.jpg" alt="Cant touch this" style="width:640px;height:480px;border:12;">';
+			echo '<img src="403.jpg" alt="Cant touch this" style="width:132px;height:80px;border:12;">';
 			header("refresh:3; url=index.php");
 			exit();
 			
