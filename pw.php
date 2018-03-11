973aa94388d0435877d60a879f0f9f454ad6147e
Henriquezla <Henriquezla@users.noreply.github.com>
2018-03-10 20:02:52 -0500

    Update pw.php

--- pw.php
+++ pw.php
@@ -1,8 +1,8 @@
 <?php
-
+//Used to generate username hash, and PW salt/Hash to manually create new users.
+//The resulting hashes/salt will be manually inserted into the users table by the DB admin.
 echo hash('sha256', 'kehoe_user@njit.edu').PHP_EOL;
 echo crypt('123456').PHP_EOL;
-
 
 
 ?>
