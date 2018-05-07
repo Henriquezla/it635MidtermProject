a62f47277cfd38e92c16418e60f8f32d925683aa
Henriquezla <Henriquezla@users.noreply.github.com>
2018-05-06 22:58:06 -0400

    Added button to view players

--- home.php
+++ home.php
@@ -1,20 +1,25 @@
-<?php 
-	include("functions.php");
-	session_start();
-	gatekeeper('User');
-	
-	if(isset($_POST['btn-logout']) ) {
-		header("Location: logout.php");
-	}
-	if(isset($_POST['btn-teams']) ) {
-		header("Location: userteams.php");
-	}
-	if(isset($_POST['btn-schedules']) ) {
-		header("Location: userschedules.php");
-	}
-	if(isset($_POST['btn-scores']) ) {
-		header("Location: userscores.php");
-	}
+<?php
+        include("functions.php");
+        session_start();
+        gatekeeper('User');
+
+        if(isset($_POST['btn-logout']) ) {
+                header("Location: logout.php");
+        }
+        if(isset($_POST['btn-teams']) ) {
+                header("Location: userteams.php");
+        }
+        if(isset($_POST['btn-schedules']) ) {
+                header("Location: userschedules.php");
+        }
+        if(isset($_POST['btn-scores']) ) {
+                header("Location: userscores.php");
+        }
+
+        if(isset($_POST['btn-players']) ) {
+                header("Location: userplayers.php");
+        }
+
 ?>
 
 
@@ -27,23 +32,25 @@
 </HEAD>
 
 <div class="container">
-	<div class="row">
-    	<div class="container" id="formContainer">
+        <div class="row">
+        <div class="container" id="formContainer">
 
           <form class="form-signin" id="userchoice" role="form" method="post" action="" autocomplete="off">
-		  <div style="margin:auto;width: 50%;padding: 40px;">
-			<h3 class="form-signin-heading">Welcome Back, <?php echo $_SESSION['f_name']; ?></h3>
+                  <div style="margin:auto;width: 50%;padding: 40px;">
+                        <h3 class="form-signin-heading">Welcome Back, <?php echo $_SESSION['f_name']; ?></h3>
             <h4 class="form-signin-heading">Choose an Option</h4><br>
             <a href="#" id="flipToRecover" class="flipLink">
             </a>
             <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-teams">View Teams</button>
-			<button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-schedules">View Schedules</button>
-			<button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-scores">View Scores</button><br>
-			<button class="btn btn-lg btn-danger btn-block" type="submit" name="btn-logout">Sign Out</button>
-			</div>
+                        <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-schedules">View Schedules</button>
+                        <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-scores">View Scores</button>
+                        <button class="btn btn-lg btn-primary btn-block" type="submit" name="btn-players">View Players</button><br>
+                        <button class="btn btn-lg btn-danger btn-block" type="submit" name="btn-logout">Sign Out</button>
+                        </div>
           </form>
-		
+
         </div>
-	</div>
+        </div>
 </div>
 </HTML>
+>
